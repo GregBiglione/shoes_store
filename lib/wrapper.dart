fe8e0a654256a3b09f609e51a8dc6514a4aff690
GregBiglione <gregory.biglione81@gmail.com>
2023-08-16 01:55:30 +0200

    Added HomeScreen, LoginScreen, SignUpScreen, Wrapper & modified main

---
+++ lib/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_store/presentation/home_screen.dart';
+import 'package:shoes_store/presentation/sign_up_screen.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  bool isLoggedIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoggedIn
+        ? const HomeScreen()
+        : const SignUpScreen();
+  }
+}
