78d7508ed6b7dc5201aefed8c6976d807c688fc5
GregBiglione <gregory.biglione81@gmail.com>
2023-10-12 00:48:24 +0200

    Added PreviousPurchaseScreen & IconButton in HomeScreen

---
+++ lib/presentation/previous_purchase/previous_purchase_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PreviousPurchaseScreen extends StatefulWidget {
+  const PreviousPurchaseScreen({super.key});
+
+  @override
+  State<PreviousPurchaseScreen> createState() => _PreviousPurchaseScreenState();
+}
+
+class _PreviousPurchaseScreenState extends State<PreviousPurchaseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
